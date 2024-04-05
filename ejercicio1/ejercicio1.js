92a43b4d7ed9ef4b81caf16b84b511a4af5043e3
[JhonF24] <je.franco16@ciaf.edu.co>
2024-04-04 20:49:05 -0500

    update6

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,1 +1,21 @@
-console.log("hello world!!");
+function esParOImpar(numero) {
+    if(numero % 2 === 0) {
+        return "El número es par";
+    } else {
+        return "El número es impar";
+    }
+}
+
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Por favor ingresa un número: ', (numero) => {
+  numero = parseInt(numero);
+  console.log(esParOImpar(numero));
+  rl.close();
+});
