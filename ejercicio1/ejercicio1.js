3b2f64d9c10e73c3efafadc80f2e1865d5a9571d
JonathanCG-94 <jhonathan9406@gmail.com>
2024-04-04 20:49:56 -0500

    Pull

--- ejercicio1/ejercicio1.js
+++ ejercicio1/ejercicio1.js
@@ -1,1 +1,11 @@
-console.log("hello world!");
+const dataFromConsole = process.argv;
+
+let num = prompt("Ingrese un Numero");
+
+num = parseInt(num);
+
+if (num % 2 === 0) {
+    console.log(num + " Es un Numero Par");
+} else {
+    console.log( num + " Es un Numero impar");
+};
